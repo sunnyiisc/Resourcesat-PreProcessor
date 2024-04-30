710b5f8aec1b13cb674e99c1feec328d533712d6
Supantha Sen <supantha_sen@nrsc.gov.in>
2024-04-30 12:41:05 +0530

    updating the angle band size automatic from the anle matrix.

--- reading_h5.py
+++ reading_h5.py
@@ -34,21 +34,27 @@ def read_h5_angles(prod_path):
 
     ##Resizing the angle bands to the Image data size
     dst_shp = data_h5['ImageData']['B2'].shape
-    sun_azi = resize(sun_azi_ang, dst_shp)
-    sun_ele = resize(sun_ele_ang, dst_shp)
-    sat_azi = resize(sat_azi_ang, dst_shp)
-    sat_ele = resize(sat_ele_ang, dst_shp)
+    sun_azi = resize(scan_pix, sun_azi_ang, dst_shp)
+    sun_ele = resize(scan_pix, sun_ele_ang, dst_shp)
+    sat_azi = resize(scan_pix, sat_azi_ang, dst_shp)
+    sat_ele = resize(scan_pix, sat_ele_ang, dst_shp)
 
     return(sat_azi, sat_ele, sun_azi, sun_ele)
 
 
-def resize(ang, dst_shp):
-    ang_mat = np.reshape(ang, (30, 28))
+def resize(scan_pix, ang, dst_shp):
+    ##Getting the size of the angle band given
+    sampling_size = scan_pix[1][1]
+    size_scan_pix = (scan_pix[-1] / sampling_size) + 1
+    size_xy = tuple( size_scan_pix.astype('int') )[::-1]
+
+    ## Creating the angle bands to 2D and interpolating to image size
+    ang_mat = np.reshape(ang, size_xy)     ##RS2=(30, 28), RS2A=(16, 14)
     ang_img = cv2.resize(ang_mat, dst_shp, interpolation=cv2.INTER_NEAREST)
     return(ang_img)
 
 
 if __name__ == '__main__':
-    prod_path = pathlib.Path('Z://DQE//cvss//Resourcesat_Product//243757811_IRS-R2_L3_05-JAN-2024_STD_GEOREF_065970_099_061//BAND.h5')
+    prod_path = pathlib.Path('C://Users//Supantha_Sen//Desktop//share//Satellite_Data//Resourcesat//243757811_IRS-R2_L3_05-JAN-2024_STD_GEOREF_065970_099_061//BAND.h5')
 
     read_h5_angles(prod_path)
