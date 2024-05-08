49d827cf4bd3b70858f0083120312286b3a33407
Supantha Sen <spnthsen@gmail.com>
2024-05-08 21:59:24 +0530

    Adding Reading Meta file, Changed the sequence of Angle bands to sun_Azi, sun_zn, sat_Azi, sat_zn.

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ from affine import Affine
 # Importing Custom Modules
 import browse_gui
 from reading_h5 import read_h5_angles
+import reading_metafile
 
 ...
 
 
 def main(prod_path, save_dir):
 
-    save_path = save_dir.joinpath(prod_path.parent.stem + '.tiff')
+    meta = reading_metafile.meta_read( prod_path.parent.joinpath('BAND_META.txt') )
+
 
     with rio.open_rasterio(prod_path, engine='h5netcdf') as dataset:
         ##Extracting Image Meta Parameters
@@ -61,24 +63,27 @@ def main(prod_path, save_dir):
 
         ##Adding Angle Bands
         sat_azi, sat_ele, sun_azi, sun_ele = read_h5_angles(prod_path)
-        dataset['Sat_Azi_Band'] = xr.DataArray(sat_azi.reshape(1, len(row), len(col)), coords=dataset.coords,
-                                               dims=dataset.dims, name='Sat_Azi_Band')
-        dataset['Sat_Ele_Band'] = xr.DataArray(sat_ele.reshape(1, len(row), len(col)), coords=dataset.coords,
-                                               dims=dataset.dims, name='Sat_Ele_Band')
         dataset['Sun_Azi_Band'] = xr.DataArray(sun_azi.reshape(1, len(row), len(col)), coords=dataset.coords,
                                                dims=dataset.dims, name='Sun_Azi_Band')
-        dataset['Sun_Ele_Band'] = xr.DataArray(sun_ele.reshape(1, len(row), len(col)), coords=dataset.coords,
-                                               dims=dataset.dims, name='Sun_Ele_Band')
+        dataset['Sun_Zn_Band'] = xr.DataArray(sun_ele.reshape(1, len(row), len(col)), coords=dataset.coords,
+                                               dims=dataset.dims, name='Sun_Zn_Band')
+        dataset['Sat_Azi_Band'] = xr.DataArray(sat_azi.reshape(1, len(row), len(col)), coords=dataset.coords,
+                                               dims=dataset.dims, name='Sat_Azi_Band')
+        dataset['Sat_Zn_Band'] = xr.DataArray(sat_ele.reshape(1, len(row), len(col)), coords=dataset.coords,
+                                               dims=dataset.dims, name='Sat_Zn_Band')
+        for band in dataset:
+            if band.startswith('S'):
+                dataset[band].attrs['scale_factor'] = 0.01
+
 
         ##Writing the Raster TIFF file
-        dataset.squeeze().rio.to_raster(save_path)
-        #dataset_reproj.squeeze().rio.to_raster(save_dir.joinpath(prod_path.parent.stem + '_reproj' + '.tiff'))
+        print('Writing DN file ... with angle bands scaling factor as 0.01 ...')
+        save_path_dn = save_dir.joinpath(prod_path.parent.stem + '.tiff')
+        dataset.squeeze().rio.to_raster(save_path_dn)
+        print('DN file written at ',save_path_dn)
 
 if __name__ == '__main__':
     prod_path = pathlib.Path(browse_gui.browse_file('Select the HDF file to be converted'))
     save_dir = pathlib.Path(browse_gui.browse_folder('Select the Folder to save the TIFF file'))
-
-    #prod_path = pathlib.Path('Z://DQE//cvss//Resourcesat_Product//243757811_IRS-R2_L3_05-JAN-2024_STD_GEOREF_065970_099_061//BAND.h5')
-    #save_dir = pathlib.Path('Z://DQE//cvss//Resourcesat_Product')
 
     main(prod_path, save_dir)
